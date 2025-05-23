566e533a38aa28e2a7ba7fb02372444f8cc70bdd
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-05-23 16:41:51 +0100

    added JSDoc comments to functions.

--- client/src/main.js
+++ client/src/main.js
@@ -1,3 +1,8 @@
+/**
+ * @function submitHandler
+ * @param {event} e event handler for form submission.
+ * @description collects form data and sends it to server as json.
+ */
 function submitHandler(e) {
   e.preventDefault();
   const dataOnPage = new FormData(bookPage);
@@ -13,14 +18,26 @@ function submitHandler(e) {
   });
 }
 
+/**
+ * @function getDate
+ * @returns date string in format 'dd/mm/YY HH:MM' but excludes leading 0s.
+ */
 function getDate() {
   const now = new Date();
   const day = now.getDate();
   const month = now.getMonth();
   const year = now.getFullYear();
-  const hrs = now.getHours;
+  const hrs = now.getHours();
+  const mins = now.getMinutes();
+  return `${day}/${month}/${year} ${hrs}:${mins}`;
 }
 
+/**
+ * @function createMessage
+ * @param {object} message the message data from the database—>server.
+ * @description creates the necessary elements for an individual message,
+ *  chevron made for mobile resolution.
+ */
 function createMessage(message) {
   const responses = document.getElementById("responses");
 
@@ -60,6 +77,11 @@ function createMessage(message) {
   responses.appendChild(msgBox);
 }
 
+/**
+ * @async
+ * @description performs GET HTTP request for guestbook messages
+ *  and loops through each to create DOM elements.
+ */
 (async () => {
   const messages = await fetch("http://localhost:8080/get_data");
   msgData = await messages.json();
